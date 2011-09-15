bb13d860b566627db31649dcdd72091eb20e6392
Arnab Kumar Datta <arnabkd@student.uio.no>
2011-09-15 10:31:13 +0200

    Finished task 4 of week 3

--- inf3331/ukesoppgaver/uke3/findallerror.py
+++ inf3331/ukesoppgaver/uke3/findallerror.py
@@ -1,7 +1,14 @@
+import re #was missing import statement
+
+#Numbers were not grouped properly together when in scientific notation format
+#Added () around (\d+(\.\d*)?|\d*\.\d+)([eE][+\-]?\d+)?
 real = \
- r"\s*(?P<number>-?(\d+(\.\d*)?|\d*\.\d+)([eE][+\-]?\d+)?)\s*"
+ r"\s*(?P<number>-?((\d+(\.\d*)?|\d*\.\d+)([eE][+\-]?\d+)?))\s*"
 c = re.compile(real)
 some_interval = "[3.58652e+05 , 6E+09]"
 groups = c.findall(some_interval)
-lower = float(groups[1][c.groupindex['number']])
-upper = float(groups[2][c.groupindex['number']])
+lower = float(groups[0][c.groupindex['number']]) #1 changed to 0
+upper = float(groups[1][c.groupindex['number']]) #2 changed to 1
+
+print "%.5e" %lower
+print "%.5e" %upper
