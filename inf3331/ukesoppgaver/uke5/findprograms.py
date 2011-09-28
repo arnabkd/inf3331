daf0b744ee8e4c2befed7f1921f511fe159b00f8
Arnab Kumar Datta <arnabkd@student.uio.no>
2011-09-28 19:44:35 +0200

    Added runtime example to 5.1 and cleaned up a bit in the code

--- inf3331/ukesoppgaver/uke5/findprograms.py
+++ inf3331/ukesoppgaver/uke5/findprograms.py
@@ -5,14 +5,13 @@ import os
 def findprograms(prog_list):
 	progs_found = {}
 	for prog in prog_list:
-		progs_found[prog] = None #set the path to none for all programs that we are searching for
+		progs_found[prog] = None #set the path to none for all programs that we are searching for (we don't know where they are installed)
 	paths = os.getenv("PATH").split(os.pathsep)
 
 	for path in paths:
 		apps_in_path = os.listdir(path)
 		for app in prog_list:
-			if app in apps_in_path:
-				progs_found[app] = path
+			if app in apps_in_path:	progs_found[app] = path
 	return progs_found
 
 
@@ -37,3 +36,10 @@ for program in installed.keys():
 
 
 # Runtime example
+#arnabkd@speedwell ~/Desktop/uni/inf3331/inf3331/ukesoppgaver/uke5 $ python findprograms.py 
+#You have convert (image conversion, part of the ImageMagick package)
+#You have gs (ghostscript, ps/pdf interpreter and previewer)
+#You have gnuplot (plotting program)
+#You have swig (generator for Python interfaces to C/C++)
+#You have f2py (generator for Python interfaces to F77)
+#*** Program xemacs was not found on the system
