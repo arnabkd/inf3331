839813bb174a49b278eb32b5116cf41d92cde855
Arnab Kumar Datta <arnabkd@student.uio.no>
2011-10-14 07:40:52 +0200

    task 6.3 done with runtime example

--- inf3331/ukesoppgaver/uke6/vectorize_integration.py
+++ inf3331/ukesoppgaver/uke6/vectorize_integration.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
-from scitools import std
+from math import *
+
 # Wikipedia gives trapezoidal rule as : 
-# f(x) dx = (b-a) * (f(a) + f(b)) / 2
+# Integral of f(x) from a to b = (h/2)*(f(a) + f(b)) + \
+#                              h*sum(i=1,...n-1) f(a+i*h)
+def trapz(f , a ,b ,steps):
+	h = (b-a)/float(steps-1.0)
+	sum = 0.0
+	for i in range(steps):
+		sum += f(a+ i*h)
+	res = h * (sum - f(a)/2 - f(b)/2)
+	return res
 
-def trapezoidal_rule(f, a, b, n):	
-	h = (b-a)/float(n)
-	sum = 0.5 * f(a)
+def f_1(x):
+	return 1+x
+
+def sqr(x):
+	return x*x
 	
-	for i in iseq(1, n-1):
-		sum += f(a + i*h)
-	sum += f(b) / 2
-	sum += h
-	
-	return sum
+print trapz(f_1 , 0, 2, 200) #outputs 4.01005025126
 
-
-print trapezoidal_rule(sin, 0 , 2*pi , 200)
+print trapz(sqr, 0, 3, 10000) #outputs 9.000 
+#integral (x^2) = (x^3)/3 -> ((3^3)/3) - ((0^3)/3) = 9 - 0 = 9
