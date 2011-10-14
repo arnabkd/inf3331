dba514fd7b914093e577d181cf76e53b0fcaa6f7
U-arnab-PC\arnab <arnab@arnab-PC.(none)>
2011-10-14 08:12:36 +0200

    see test2.py for automation hint...SIGH

--- inf3331/ukesoppgaver/uke6/test2.py
+++ inf3331/ukesoppgaver/uke6/test2.py
@@ -1,11 +1,6 @@
 import sys
-# from subprocess import *
 
-# args = sys.argv[1:]
-# src = 'bar.py'
-print sys.argv[1]
-print sys.argv[2]
-# import bar; print 'via import',; bar.foo(sys.argv[1], sys.argv[2])
-import bar; print 'via import',; bar.foo(sys.argv[1],sys.argv[2])
+argslist = ('2','3')
+import bar
 
-# bar.main(args.split(' '))
+print bar.foo(2,3)
