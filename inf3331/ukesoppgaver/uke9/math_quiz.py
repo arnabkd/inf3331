4e20ffc9ecb0db4d02487bdf4f4445bdfa944332
arnab <arnab@arnab-ThinkPad-Z61t.(none)>
2011-11-02 02:38:57 +0100

    quiz almost working, need to scale points properly to rounds. Also need runtime example

--- inf3331/ukesoppgaver/uke9/math_quiz.py
+++ inf3331/ukesoppgaver/uke9/math_quiz.py
@@ -1,5 +1,5 @@
 import operator,random
-messages = {0:'Please ask your math teacher for help!', 1:'You need more practice.', 2:'Well done!'}
+messages = ['','Please ask your math teacher for help!', 'You need more practice.','Well done!']
 levels = {'B' : 10 , 'I': 25 , 'A':100}
 func = {'add': operator.add, 'mul' : operator.mul, 'div' : operator.div , 'sub' : operator.sub}
 ops = {func['add'] : '+' , func['mul'] : '*', func['div'] : '/' , func['sub'] : '-'}
@@ -7,7 +7,7 @@ ops = {func['add'] : '+' , func['mul'] : '*', func['div'] : '/' , func['sub'] : 
 def f(op,x,y):
     return op(x,y)
 
-def play(level, op):
+def play(level, op = random.choice(func.values())):
     operand1 = random.randint(1,level);    operand2 = random.randint(1,level)
     ans = f(op,operand1, operand2)
     input = int(raw_input(('Enter your answer for %d %s %d : ')%(operand1, ops[op],operand2 )))
@@ -17,19 +17,19 @@ def play(level, op):
 def main():
     level = levels[raw_input("Choose difficulty : (B)eginner / (I)ntermediate / (A)dvanced: ")]
     rounds = int (raw_input("Enter how many rounds you would like to play: "))
-    type = (raw_input("What type of quiz do you want : div/mul/sub/add/mix"))
+    type = (raw_input("What type of quiz do you want (note : division is integer division) :\ndiv/mul/sub/add/mix : "))
 
     points = 0
     
     if type != "mix":
-        for i in range (rounds) :
-            points += play(level,func[type])
+        for i in range (rounds) : points += play(level,func[type])
     else :
-        for i in range (rounds) :
-            points += play(level, random.choice(func.keys()))
+        for i in range (rounds) : points += play(level)
 
-    print "You got %d out of %d possible!"%(points,rounds)
     
+    print messages[x]
+    if (raw_input("Type \'r\' and press enter to restart the quiz\n")) == 'r' :
+        main()
 
 if __name__ == "__main__":
     main()
