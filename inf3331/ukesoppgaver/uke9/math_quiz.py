b84d3a0d93ec7177449ce0ac413ea2678b6bc763
Arnab Datta <arnabkd@student.uio.no>
2011-11-02 12:24:58 +0100

    9.5 complete, need runtime example

--- inf3331/ukesoppgaver/uke9/math_quiz.py
+++ inf3331/ukesoppgaver/uke9/math_quiz.py
@@ -1,23 +1,31 @@
 import operator,random
-messages = ['','Please ask your math teacher for help!', 'You need more practice.','Well done!']
 levels = {'B' : 10 , 'I': 25 , 'A':100}
-func = {'add': operator.add, 'mul' : operator.mul, 'div' : operator.div , 'sub' : operator.sub}
-ops = {func['add'] : '+' , func['mul'] : '*', func['div'] : '/' , func['sub'] : '-'}
-
-def f(op,x,y):
-    return op(x,y)
+func = {'add': operator.add, 'mul' : operator.mul, 'sub' : operator.sub}
+ops = {func['add'] : '+' , func['mul'] : '*', func['sub'] : '-'}
+messages = ['Please ask your math teacher for help!','You need more practice.',\
+                'Well done!']
 
 def play(level, op = random.choice(func.values())):
     operand1 = random.randint(1,level);    operand2 = random.randint(1,level)
-    ans = f(op,operand1, operand2)
-    input = int(raw_input(('Enter your answer for %d %s %d : ')%(operand1, ops[op],operand2 )))
-    return (1 if input == ans else 0)
-    
+    ans = op(operand1, operand2)
+    input = int(raw_input(('Enter your answer for %d %s %d : ')\
+                              %(operand1, ops[op],operand2 )))
+    return 1 if input == ans else 0
+
+def feedback(points,rounds):
+    ratio = float(points) / float(rounds)
+    if (ratio > 2.0/3.0): #more than 2/3rd correct
+        print messages[2]
+    elif(ratio >= 1.0/3.0): #more than, or exactly 1/3rd correct
+        print messages[1]
+    else: #less than 1/3rd correct
+        print messages[0]    
 
 def main():
-    level = levels[raw_input("Choose difficulty : (B)eginner / (I)ntermediate / (A)dvanced: ")]
+    level = levels[raw_input("Choose difficulty (B)eginner / (I)ntermediate"+\
+                              " / (A)dvanced: ")]
     rounds = int (raw_input("Enter how many rounds you would like to play: "))
-    type = (raw_input("What type of quiz do you want (note : division is integer division) :\ndiv/mul/sub/add/mix : "))
+    type = (raw_input("What type of quiz do you want :\nmul/sub/add/mix : "))
 
     points = 0
     
@@ -26,8 +34,8 @@ def main():
     else :
         for i in range (rounds) : points += play(level)
 
-    
-    print messages[x]
+    feedback(points,rounds)
+
     if (raw_input("Type \'r\' and press enter to restart the quiz\n")) == 'r' :
         main()
 
